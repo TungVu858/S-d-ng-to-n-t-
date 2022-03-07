6f583bd060b82b77015c4171581328bca755e73f
TungVu858 <tungvt858@gmail.com>
2022-03-07 17:39:23 +0700

    Bai tap bien,kieu du lieu & toan tu

--- js.js
+++ js.js
@@ -4,3 +4,7 @@ let width = parseInt(inputWidth);
 let height = parseInt(inputHeight);
 let area = width * height;
 document.write("The area is:" +area);
+let a = "1";
+let b = 2;
+let c = 4;
+document.write("d"+(a+b+c))
